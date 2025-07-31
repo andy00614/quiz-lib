d831deadcfee5bba6025b99813dd3023eaeaf82e
andy00614 <33732843@qq.com>
2025-07-31 11:06:49 +0800

    feat: [tmp] no cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,8 @@ app = FastAPI(
 # 配置CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
+    # allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
